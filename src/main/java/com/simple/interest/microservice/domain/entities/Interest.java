f45449c29e2fe007c1d12bb8e7547224ce7ab7e0
alfonsovgs <vc.alfonso@htomail.com>
2023-03-25 17:59:53 -0600

    refactor: change data type

--- src/main/java/com/simple/interest/microservice/domain/entities/Interest.java
+++ src/main/java/com/simple/interest/microservice/domain/entities/Interest.java
@@ -1,5 +1,6 @@
 package com.simple.interest.microservice.domain.entities;
 
+import java.time.LocalDate;
 import java.util.Date;
 
-public record Interest(int paymentNumber, double amount, Date paymentDate) { }
+public record Interest(int paymentNumber, double amount, LocalDate paymentDate) { }
