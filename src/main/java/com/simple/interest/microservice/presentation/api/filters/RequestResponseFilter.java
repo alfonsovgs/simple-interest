5a716127f50c5c838bd0a7e8f4b910a482bf7900
alfonsovgs <vc.alfonso@htomail.com>
2023-03-25 23:20:27 -0600

    feat: implemented filter to storage request/response

---
+++ src/main/java/com/simple/interest/microservice/presentation/api/filters/RequestResponseFilter.java
@@ -0,0 +1,57 @@
+package com.simple.interest.microservice.presentation.api.filters;
+
+
+import com.simple.interest.microservice.infrastructure.data.RequestResponseStorage;
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.util.ContentCachingRequestWrapper;
+import org.springframework.web.util.ContentCachingResponseWrapper;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+
+
+@Component
+public class RequestResponseFilter implements Filter {
+    private final RequestResponseStorage storage;
+
+    public RequestResponseFilter(RequestResponseStorage storage) {
+        this.storage = storage;
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+
+        ContentCachingRequestWrapper requestWrapper = new ContentCachingRequestWrapper(request);
+        ContentCachingResponseWrapper responseWrapper = new ContentCachingResponseWrapper(response);
+
+        filterChain.doFilter(requestWrapper, responseWrapper);
+
+        String requestBody = getStringValue(requestWrapper.getContentAsByteArray(),
+                request.getCharacterEncoding());
+
+        String responseBody = getStringValue(responseWrapper.getContentAsByteArray(),
+                response.getCharacterEncoding());
+
+        if(responseWrapper.getStatus() == HttpStatus.OK.value() ) {
+            storage.save(requestBody, responseBody);
+        }
+
+        responseWrapper.copyBodyToResponse();
+    }
+
+    private String getStringValue(byte[] contentAsByteArray, String characterEncoding) {
+        try {
+            return new String(contentAsByteArray, 0, contentAsByteArray.length, characterEncoding);
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+
+        return "";
+    }
+}
